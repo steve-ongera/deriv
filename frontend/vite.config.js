0aab29c01a8ce9b4b47258a3c6a425afe4b6b37b
steve-ongera <gadafisteve001@gmail.com>
2026-07-21 03:11:29 +0300

    under development

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        open: true,
+        proxy: {
+            "/api": {
+                target: "http://127.0.0.1:8000",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+        emptyOutDir: true,
+    },
+});
